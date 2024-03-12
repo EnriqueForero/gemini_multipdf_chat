0c1a8921a2ad342352cf170819a879b7a471595f
EnriqueForero <43864186+EnriqueForero@users.noreply.github.com>
2024-03-12 08:21:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def main():
                 st.success("Done")
 
     # Main content area for displaying chat messages
-    st.title("Chat with PDF files using Gemini🤖")
+    st.title("Chat with PDF files using Gemini Pro 🤖")
     st.write("Welcome to the chat!")
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
